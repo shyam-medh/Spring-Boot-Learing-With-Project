6aa5a73d38e3b08637297048d89cc6c6bc5a4ed8
shyam-medh <baranwal07shyam@gmail.com>
2026-09-21 01:14:35 +0530

    feat: init spring boot project and hello controller

---
+++ backend/src/test/java/com/devopshub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopshub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
